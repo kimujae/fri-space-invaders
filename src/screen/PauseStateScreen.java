7542c15e2187ba2ea31469eb55a421e8e658c3b0
yppp33 <ysp051@hanyang.ac.kr>
2022-11-11 17:14:21 +0900

    test

--- src/screen/PauseStateScreen.java
+++ src/screen/PauseStateScreen.java
@@ -78,6 +78,7 @@ public class PauseStateScreen extends Screen{
                         SoundPlay.getInstance().stopBgm();
                         this.isRunning = false;
                     }
+                    if (menuCode == 2) this.isRunning = false;
                     if (menuCode == 1) this.isRunning = false;
                 }
             }
@@ -89,7 +90,7 @@ public class PauseStateScreen extends Screen{
         private void nextMenuItem() {
             if (this.menuCode == 1) {
                 menuCode++;
-                //이수빈 save returnCode작성
+                returnCode = 12;
             }
             else if (this.menuCode == 2) {
                 menuCode++;
@@ -115,7 +116,7 @@ public class PauseStateScreen extends Screen{
             }
             else if(this.menuCode ==3) {
                 menuCode--;
-                //이수빈 save returnCode작성
+                returnCode = 12;
             }
             else if(this.menuCode ==4) {
                 menuCode--;
