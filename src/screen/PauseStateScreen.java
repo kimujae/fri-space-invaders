835fdf93e99a251395b248d50218f7bb5a40e11b
kimujae <62458513+kimujae@users.noreply.github.com>
2022-11-30 11:59:14 +0900

    Merge pull request #40 from kimujae/lsb_bugfix
    Lsb bugfix

--- src/screen/PauseStateScreen.java
+++ src/screen/PauseStateScreen.java
@@ -73,12 +73,12 @@ public class PauseStateScreen extends Screen{
                 }
 
                 if (inputManager.isKeyDown(KeyEvent.VK_SPACE)) {
-                    if (menuCode == 4) System.exit(0);
-                    if (menuCode == 3) {
+                    if (menuCode == 3) System.exit(0);
+                    if (menuCode == 2) {
                         SoundPlay.getInstance().stopBgm();
                         this.isRunning = false;
                     }
-                    if (menuCode == 1 || menuCode == 2) this.isRunning = false;
+                    if (menuCode == 1) this.isRunning = false;
                 }
             }
         }
@@ -87,41 +87,64 @@ public class PauseStateScreen extends Screen{
          * Shifts the focus to the next menu item.
          */
         private void nextMenuItem() {
-            if (this.menuCode == 1) {
-                menuCode++;
-                returnCode = 5;
-            }
-            else if (this.menuCode == 2) {
-                menuCode++;
-                returnCode = 1;
-            }
-            else if(this. menuCode ==3) {
-                menuCode++;
-            }
-            else if(this. menuCode ==4) {
+//            if (this.menuCode == 1) {
+//                menuCode++;
+//                returnCode = 5;
+//            }
+//            else if (this.menuCode == 2) {
+//                menuCode++;
+//                returnCode = 1;
+//            }
+//            else if(this. menuCode ==3) {
+//                menuCode++;
+//            }
+//            else if(this. menuCode ==4) {
+//                menuCode = 1;
+//                returnCode = 2;
+//            }
+            if (menuCode == 3) {
                 menuCode = 1;
                 returnCode = 2;
+            }
+            else if (menuCode == 1) {
+                menuCode = 2;
+                returnCode = 1;
+            }
+            else {
+                menuCode = 3;
             }
         }
 
         private void previousMenuItem() {
-            if (this.menuCode == 1) {
-                menuCode = 4;
-
-            }
-            else if (this.menuCode == 2) {
-                menuCode--;
-                returnCode = 2;
-            }
-            else if(this.menuCode ==3) {
-                menuCode--;
-                returnCode =5;
-            }
-            else if(this.menuCode ==4) {
-                menuCode--;
+//            if (this.menuCode == 1) {
+//                menuCode = 4;
+//
+//            }
+//            else if (this.menuCode == 2) {
+//                menuCode--;
+//                returnCode = 2;
+//            }
+//            else if(this.menuCode ==3) {
+//                menuCode--;
+//                returnCode =5;
+//            }
+//            else if(this.menuCode ==4) {
+//                menuCode--;
+//                returnCode = 1;
+//            } 여기도 해주
+//
+//          세요
+            if (menuCode == 3) {
+                menuCode = 2;
                 returnCode = 1;
             }
-
+            else if (menuCode == 2) {
+                menuCode = 1;
+                returnCode = 2;
+            }
+            else {
+                menuCode = 3;
+            }
         }
         /**
          * Draws the elements associated with the screen.
